a9205ce78fb7476ac483e6b9ce86097877678024
gill-jaskiran <156838272+gill-jaskiran@users.noreply.github.com>
2024-11-07 09:52:31 -0500

    COMP3095 - Week 10 - Day 2

--- microservice-parent/settings.gradle.kts
+++ microservice-parent/settings.gradle.kts
@@ -1,3 +1,3 @@
 rootProject.name = "microservice-parent"
 
-include("product-service", "order-service", "inventory-service")
+include("product-service", "order-service", "inventory-service", "api-gateway")
