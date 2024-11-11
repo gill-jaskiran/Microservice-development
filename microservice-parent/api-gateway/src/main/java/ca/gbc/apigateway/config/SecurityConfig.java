c3db8c5d9246903a3a817effc8530b8015385e99
gill-jaskiran <156838272+gill-jaskiran@users.noreply.github.com>
2024-11-11 10:38:27 -0500

    COMP3095 - Week 11 - Day 1

---
+++ microservice-parent/api-gateway/src/main/java/ca/gbc/apigateway/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package ca.gbc.apigateway.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Slf4j
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+
+        log.info("Initializing Security Filter Chain....");
+
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)               // Disable CSRF protection
+               /* .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll())  */              // All requests temporarily permitted
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated())             // All requests require authentication
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults()))
+                .build();
+
+    }
+
+
+
+
+
+}
