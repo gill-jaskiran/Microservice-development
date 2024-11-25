f7ad58008da3df69d0b767b02e8325add1c982d1
gill-jaskiran <156838272+gill-jaskiran@users.noreply.github.com>
2024-11-25 09:50:16 -0500

    COMP3095 - Week 13 - Day 1

--- microservice-parent/api-gateway/src/main/java/ca/gbc/apigateway/routes/Routes.java
+++ microservice-parent/api-gateway/src/main/java/ca/gbc/apigateway/routes/Routes.java
@@ -2,15 +2,20 @@ package ca.gbc.apigateway.routes;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
+import java.net.URI;
+
 import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
 
 @Configuration
 @Slf4j
@@ -30,21 +35,15 @@ public class Routes {
 
         log.info("Initializing product-service route with URL: {}", productServiceUrl);
 
-        return GatewayRouterFunctions.route("product_service")
+        return route("product_service")
                 .route(RequestPredicates.path("/api/product"), request -> {
                     log.info("Received request for product-service: {}", request.uri());
-
-                    try{
-                        ServerResponse response = HandlerFunctions.http(productServiceUrl).handle(request);
-                        log.info("Response status: {}", response.statusCode());
-                        return response;
-                    }catch(Exception e){
-                        log.error("Error occurred while routing to: {}", e.getMessage(),e);
-                        return ServerResponse.status(500).body("Error occurred while routing to product-service");
-                    }
-
+                    // week 13 refactor for the rest
+                        return HandlerFunctions.http(productServiceUrl).handle(request);
 
                 })
+                .filter(CircuitBreakerFilterFunctions
+                        .circuitBreaker("productServiceCircuitBreaker", URI.create("forward:/fallbackRoute")))
 
                 .build();
 
@@ -56,21 +55,16 @@ public class Routes {
     public RouterFunction<ServerResponse> orderServiceRoute() {
         log.info("Initializing order-service route with URL: {}", orderServiceUrl);
 
-        return GatewayRouterFunctions.route("order_service")
+        return route("order_service")
                 .route(RequestPredicates.path("/api/order"), request -> {
                     log.info("Received request for order-service: {}", request.uri());
 
-                    try{
-                        ServerResponse response = HandlerFunctions.http(orderServiceUrl).handle(request);
-                        log.info("Response status: {}", response.statusCode());
-                        return response;
-                    }catch(Exception e){
-                        log.error("Error occurred while routing to: {}", e.getMessage(),e);
-                        return ServerResponse.status(500).body("Error occurred while routing to order-service");
-                    }
-
+                    // week 13 refactor for the rest
+                    return HandlerFunctions.http(orderServiceUrl).handle(request);
 
                 })
+                .filter(CircuitBreakerFilterFunctions
+                        .circuitBreaker("orderServiceCircuitBreaker", URI.create("forward:/fallbackRoute")))
 
                 .build();
 
@@ -81,34 +75,30 @@ public class Routes {
     public RouterFunction<ServerResponse> inventoryServiceRoute() {
         log.info("Initializing order-service route with URL: {}", inventoryServiceUrl);
 
-        return GatewayRouterFunctions.route("inventory_service")
+        return route("inventory_service")
                 .route(RequestPredicates.path("/api/inventory"), request -> {
                     log.info("Received request for inventory-service: {}", request.uri());
 
-                    try{
-                        ServerResponse response = HandlerFunctions.http(inventoryServiceUrl).handle(request);
-                        log.info("Response status: {}", response.statusCode());
-                        return response;
-                    }catch(Exception e){
-                        log.error("Error occurred while routing to: {}", e.getMessage(),e);
-                        return ServerResponse.status(500).body("Error occurred while routing to inventory-service");
-                    }
-
+                    // week 13 refactor for the rest
+                    return HandlerFunctions.http(inventoryServiceUrl).handle(request);
 
                 })
+                .filter(CircuitBreakerFilterFunctions
+                        .circuitBreaker("inventoryServiceCircuitBreaker", URI.create("forward:/fallbackRoute")))
 
                 .build();
-
 
     }
 
     @Bean
     public RouterFunction<ServerResponse> productServiceSwaggerRoute() {
 
-        return GatewayRouterFunctions.route("product_service_swagger")
+        return route("product_service_swagger")
                 .route(RequestPredicates.path("/aggregate/product-service/v3/api-docs"),
                         HandlerFunctions.http(productServiceUrl))
                 .filter(setPath("/api-docs"))
+                .filter(CircuitBreakerFilterFunctions
+                        .circuitBreaker("productSwaggerCircuitBreaker", URI.create("forward:/fallbackRoute")))
                 .build();
 
     }
@@ -116,10 +106,12 @@ public class Routes {
     @Bean
     public RouterFunction<ServerResponse> orderServiceSwaggerRoute() {
 
-        return GatewayRouterFunctions.route("order_service_swagger")
+        return route("order_service_swagger")
                 .route(RequestPredicates.path("/aggregate/order-service/v3/api-docs"),
                         HandlerFunctions.http(orderServiceUrl))
                 .filter(setPath("/api-docs"))
+                .filter(CircuitBreakerFilterFunctions
+                        .circuitBreaker("orderSwaggerCircuitBreaker", URI.create("forward:/fallbackRoute")))
                 .build();
 
     }
@@ -127,12 +119,24 @@ public class Routes {
     @Bean
     public RouterFunction<ServerResponse> inventoryServiceSwaggerRoute() {
 
-        return GatewayRouterFunctions.route("inventory_service_swagger")
+        return route("inventory_service_swagger")
                 .route(RequestPredicates.path("/aggregate/inventory-service/v3/api-docs"),
                         HandlerFunctions.http(inventoryServiceUrl))
                 .filter(setPath("/api-docs"))
+                .filter(CircuitBreakerFilterFunctions
+                        .circuitBreaker("inventorySwaggerCircuitBreaker", URI.create("forward:/fallbackRoute")))
                 .build();
 
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> fallbackRoute() {
+
+        return route("fallbackRoute")
+                .route(RequestPredicates.all(),
+                        request ->ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
+                                .body("Service is Temporarily Unavailable, try again later"))
+                .build();
     }
 
 
