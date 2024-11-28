582984fc56ab588f6b609886402639c66fdc0241
gill-jaskiran <156838272+gill-jaskiran@users.noreply.github.com>
2024-11-28 09:47:00 -0500

    COMP3095 - Week 13 - Day 2

--- microservice-parent/order-service/src/main/java/ca/gbc/orderservice/config/RestClientConfig.java
+++ microservice-parent/order-service/src/main/java/ca/gbc/orderservice/config/RestClientConfig.java
@@ -2,11 +2,16 @@ package ca.gbc.orderservice.config;
 
 import ca.gbc.orderservice.client.InventoryClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
@@ -19,6 +24,8 @@ public class RestClientConfig {
 
         RestClient restClient = RestClient.builder()
                 .baseUrl(InventoryServiceUrl)
+                //Week 12 6.2
+                .requestFactory(getClientRequestFactory())
                 .build();
 
 
@@ -26,6 +33,15 @@ public class RestClientConfig {
         var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
         return httpServiceProxyFactory.createClient(InventoryClient.class);
 
+
+    }
+
+    // Week 12 6.2
+    private ClientHttpRequestFactory getClientRequestFactory() {
+        ClientHttpRequestFactorySettings clientHttpRequestFactorySettings = ClientHttpRequestFactorySettings.DEFAULTS
+                .withConnectTimeout(Duration.ofSeconds(3))
+                .withReadTimeout(Duration.ofSeconds(3));
+        return ClientHttpRequestFactories.get(clientHttpRequestFactorySettings);
 
     }
 
